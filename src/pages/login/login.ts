c22e59ae1366afe8ec40ea3ad29158857918fa6c
MarioSalinas <mario_1605@live.com.mx>
2018-12-05 16:03:44 -0600

    Register login with Firebase

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthenticationProvider } from '../../providers/authentication/authentication';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,12 +17,72 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
   operation: string = 'login';
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  name: string = 'name'
+  email: string = 'email'
+  password: string = 'password'
+  password2: string = 'password2'
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private authenticationProvider: AuthenticationProvider, private alertCtrl: AlertController) {
     this.operation = 'login'
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  loginin(){
+    let alert = this.alertCtrl.create({
+      title: 'Perfecto!',
+      subTitle: 'Te has loggeado de manera correcta',
+      buttons: ['Ok']
+    });
+    let alertError = this.alertCtrl.create({
+      title: 'Ups!',
+      subTitle: 'Hubo un error',
+      buttons: ['Ok']
+    });
+
+    this.authenticationProvider.loginWithEmail(this.email, this.password)
+    .then(
+      (data)=>{
+        alert.present();
+        console.log(data)
+      }
+    ).catch(
+      (error)=>{
+        alertError.present();
+        console.log(error)
+      }
+    )
+
+  }
+
+  register(){
+    let alert = this.alertCtrl.create({
+      title: 'Perfecto!',
+      subTitle: 'Te has registrado de manera correcta',
+      buttons: ['Ok']
+    });
+    let alertError = this.alertCtrl.create({
+      title: 'Ups!',
+      subTitle: 'Hubo un error',
+      buttons: ['Ok']
+    });
+
+    this.authenticationProvider.registerWithEmail(this.email, this.password)
+    .then(
+      (data)=>{
+        alert.present();
+        console.log(data)
+      }
+    ).catch(
+      (error)=>{
+        alertError.present();
+        console.log(error)
+      }
+    )
+
   }
 
 }
